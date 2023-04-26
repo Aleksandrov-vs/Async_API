d602da531fade58feb929f314d189cd8b060755a
vasya <dfcz200115@mail.ru>
2023-04-27 03:49:47 +0600

    add (film search): добавил поиск данных в elastic по фильмам с учетом того, что нет пока индекса с жанрами

--- async_api/src/models/base.py
+++ async_api/src/models/base.py
@@ -9,8 +9,6 @@ def orjson_dumps(v, *, default):
 
 
 class BaseOrjsonModel(BaseModel):
-    uuid: uuid.UUID
-
     class Config:
         json_loads = orjson.loads
         json_dumps = orjson_dumps
--- async_api/src/models/film.py
+++ async_api/src/models/film.py
@@ -1,18 +1,77 @@
+import uuid
+from uuid import UUID
+import orjson
+
+from typing import List, Tuple, Dict
+# Используем pydantic для упрощения работы при перегонке данных из json в объекты
+from pydantic import Field
+
+from db.models.elastic_models import SerializedFilm
 from .base import BaseOrjsonModel
 
 
-class Film(BaseOrjsonModel):
-    """Полная информация по фильму."""
-    title: str
-    imdb_rating: float
-    description: str | None
-    genre: list[dict[str, str]] | None
-    actors: list[dict[str, str]] | None
-    writers: list[dict[str, str]] | None
-    directors: list[dict[str, str]] | None
+def orjson_dumps(v, *, default):
+    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
+    return orjson.dumps(v, default=default).decode()
 
 
-class Films(BaseOrjsonModel):
-    """Модель главной страницы."""
+class UUIDMixin(BaseOrjsonModel):
+    uuid: UUID
+
+
+class Actor(UUIDMixin):
+    full_name: str
+
+
+class Writer(Actor):
+    pass
+
+
+#НАДО ИСПРАВИТЬ (можно просто наследоваться от UUIDMixin), сейчас нет индексов жанров и персон
+class Director(BaseOrjsonModel):
+    uuid: UUID = Field(default=uuid.uuid4)
+    full_name: str
+
+
+#НАДО ИСПРАВИТЬ (можно просто наследоваться от UUIDMixin),, сейчас нет индексов жанров и персон
+class Genre(BaseOrjsonModel):
+    uuid: UUID = Field(default=uuid.uuid4)
+    name: str
+
+
+class ShortFilm(UUIDMixin):
     title: str
     imdb_rating: float
+
+    @classmethod
+    def from_serialized_movie(cls, serialized_movie: SerializedFilm):
+        return cls(
+            uuid=serialized_movie.id,
+            title=serialized_movie.title,
+            imdb_rating=serialized_movie.imdb_rating,
+        )
+
+
+class DetailFilm(ShortFilm):
+    description: str
+    actors: List[Actor]
+    writers: List[Writer]
+    directors: List[Director]
+    genre: List[Genre]
+
+    @classmethod
+    def from_serialized_movie(cls, serialized_movie: SerializedFilm):
+        actors = [Actor(uuid=actor.get('id'), full_name=actor.get('name')) for actor in serialized_movie.actors]
+        writers = [Writer(uuid=writer.get('id'), full_name=writer.get('name')) for writer in serialized_movie.writers]
+        directors = [Director(full_name=director[0]) for director in serialized_movie.director]
+        genre = [Genre(name=g) for g in serialized_movie.genre]
+        return cls(
+            uuid=serialized_movie.id,
+            title=serialized_movie.title,
+            imdb_rating=serialized_movie.imdb_rating,
+            description=serialized_movie.description,
+            genre=genre,
+            actors=actors,
+            writers=writers,
+            directors=directors,
+        )
