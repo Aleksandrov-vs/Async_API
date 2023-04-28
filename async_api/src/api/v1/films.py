d3d3fe6d5882160dfb86b8afc6d543ad9fa503e2
vasya <dfcz200115@mail.ru>
2023-04-28 23:48:36 +0600

    flake8 and isort

--- async_api/src/api/v1/films.py
+++ async_api/src/api/v1/films.py
@@ -4,6 +4,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
+
 from services.film import FilmService, get_film_service
 
 router = APIRouter()
@@ -27,37 +28,58 @@ class FilmSearch(BaseModel):
 
 
 @router.get('/search', response_model=List[FilmSearch])
-async def film_search(query: str,
-                      page_size: int = Query(50, gt=0, le=100,
-                                             description="Количество записей на странице (от 1 до 100)."),
-                      page_number: int = Query(1, gt=0, description="Номер страницы (начиная с 1)."),
-                      film_service: FilmService = Depends(get_film_service)) -> List[FilmSearch]:
+async def film_search(
+        query: str,
+        page_size: int = Query(
+            50, gt=0, le=100,
+            description="Количество записей на странице (от 1 до 100)."
+        ),
+        page_number: int = Query(
+            1, gt=0, description="Номер страницы (начиная с 1)."
+        ),
+        film_service: FilmService = Depends(get_film_service)
+) -> List[FilmSearch]:
 
     films = await film_service.get_by_query(query, page_size, page_number)
     if not films:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='films not found')
     return [FilmSearch(**film.dict()) for film in films]
 
 
 @router.get('/{film_id}', response_model=Film)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+async def film_details(
+        film_id: str,
+        film_service: FilmService = Depends(get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='film not found')
     return Film(**film.dict())
 
 
 @router.get('/', response_model=List[FilmSearch])
-async def films_sort(sort: str = Query('-imdb_rating', regex='^-imdb_rating$|^imdb_rating$',
-                                       description="Сортировка в формате поле-порядок (-по убыванию, без знака - по "
-                                                   "возрастанию)."), 
-                     page_size: int = Query(50, gt=0, le=100,
-                                            description="Количество записей на странице (от 1 до 100)."),
-                     page_number: int = Query(1, gt=0, description="Номер страницы (начиная с 1)."),
-                     genre_id: UUID | None = Query(None, description="id Жанра"),
-                     film_service: FilmService = Depends(get_film_service)) \
+async def films_sort(
+        sort: str = Query(
+            '-imdb_rating', regex='^-imdb_rating$|^imdb_rating$',
+            description="Сортировка в формате поле-порядок (-по убыванию, "
+                        "без знака - по возрастанию)."
+        ),
+        page_size: int = Query(
+            50, gt=0, le=100,
+            description="Количество записей на странице (от 1 до 100)."
+        ),
+        page_number: int = Query(
+            1, gt=0, description="Номер страницы (начиная с 1)."
+        ),
+        genre_id: UUID | None = Query(
+            None, description="id Жанра"
+        ),
+        film_service: FilmService = Depends(get_film_service)) \
         -> List[FilmSearch]:
-    films = await film_service.get_by_sort(sort, page_size, page_number, genre_id)
+    films = await film_service.get_by_sort(sort, page_size,
+                                           page_number, genre_id)
     if not films:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='film not found')
     return [FilmSearch(**film.dict()) for film in films]
--- async_api/src/models/base.py
+++ async_api/src/models/base.py
@@ -1,5 +1,3 @@
-import uuid
-
 import orjson
 from pydantic import BaseModel
 
